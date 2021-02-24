0273ca53e7676f8f933c2cb118cfdf6b82b4b681
Sam Darbeh <56693052+sdarbeh@users.noreply.github.com>
2021-02-24 13:31:10 -0600

    Hash routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import { appRoutes } from './utils/routes'
 
 // import { ContactRoute } from './gaurds'
