8484f72b30528a740e0834feca61415da5ad8e7f
Sam Darbeh <56693052+sdarbeh@users.noreply.github.com>
2021-02-24 13:12:31 -0600

    Updates

---
+++ precache-manifest.aa9e88cd285ad4932f9d4aca0e99513a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2824853499a41d131d5633850eceb6",
+    "url": "//sdarbeh.github.io/babyshower/index.html"
+  },
+  {
+    "revision": "29f6cf65e557bb912c14",
+    "url": "//sdarbeh.github.io/babyshower/static/css/main.37906b09.chunk.css"
+  },
+  {
+    "revision": "225d5b35e85876b016e0",
+    "url": "//sdarbeh.github.io/babyshower/static/js/2.ee22c619.chunk.js"
+  },
+  {
+    "revision": "7356ba70cb46a9043b2a42861c2b437b",
+    "url": "//sdarbeh.github.io/babyshower/static/js/2.ee22c619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f6cf65e557bb912c14",
+    "url": "//sdarbeh.github.io/babyshower/static/js/main.4eaf73d1.chunk.js"
+  },
+  {
+    "revision": "fe1b8e5a52e2dec64eb0",
+    "url": "//sdarbeh.github.io/babyshower/static/js/runtime-main.ec825ff1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/https://https://sdarbeh.github.io/babyshower/precache-manifest.a50b9b958b6cf37fb115e07bea1f4762.js"
+  "//sdarbeh.github.io/babyshower/precache-manifest.aa9e88cd285ad4932f9d4aca0e99513a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https://https://sdarbeh.github.io/babyshower/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//sdarbeh.github.io/babyshower/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
